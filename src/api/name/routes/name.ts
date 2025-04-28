2fc79181408b6862eada0f18573f2c6fcc234491
Divyam Thakur <thakurd398@gmail.com>
2025-04-28 11:26:43 +0530

    new changes

---
+++ src/api/name/routes/name.ts
@@ -0,0 +1,7 @@
+/**
+ * name router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::name.name');
---
+++ src/api/name/services/name.ts
@@ -0,0 +1,7 @@
+/**
+ * name service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::name.name');
